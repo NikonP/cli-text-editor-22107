b8b07749f17b019b7e71366f49a54dce4be3e0e0
Nikon Podgorny <podgorny.nikon@yandex.ru>
2020-02-14 17:53:02 +0300

    Пункт 6
    Отображение позиции курсора символом "|"

--- show.c
+++ show.c
@@ -8,6 +8,7 @@
 
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
 #include "common.h"
 #include "text/text.h"
 
@@ -29,12 +30,22 @@ static void show_line(int index, char *contents, int cursor, void *data)
 {
     /* Функция обработчик всегда получает существующую строку */
     assert(contents != NULL);
-    
+
     /* Декларируем неиспользуемые параметры */
     UNUSED(index);
-    UNUSED(cursor);
     UNUSED(data);
-    
+
+    /* Проверяем стоит ли курсор на текущей строке */
+    if(cursor != -1) {
+        /* Устанавливаем курсор в заданную позицию в строке*/
+        contents[cursor] = '|';
+
+        /* Если курсор в конце строки, то после него необходимо добавить \n */
+        if(cursor == (int) strlen(contents) - 1) {
+            contents[cursor + 1] = '\n';
+        }
+    }
+
     /* Выводим строку на экран */
     printf("%s", contents);
 }
